5d31566243389f4b345943d98370b3ffa142ce2b
Behzad Aslani <behzadaslani80@gmail.com>
2023-11-05 15:43:34 +0330

    feat(App)
    setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import SubmitPage from "./pages/SubmitPage"
+import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/submit" element={<SubmitPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
