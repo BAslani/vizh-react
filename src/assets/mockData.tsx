52dbeb3d56f8b813bda0cf5c6c7569d8194cb7b8
Behzad Aslani <behzadaslani80@gmail.com>
2023-11-05 19:16:02 +0330

    chore(mockData) completed mockData

--- src/assets/mockData.tsx
+++ src/assets/mockData.tsx
@@ -9,62 +9,72 @@ import nft08 from '../assets/nft08.png'
 import nft09 from '../assets/nft09.png'
 import nft10 from '../assets/nft10.png'
 
-const nftData: { nftName: string, artistName: string, currentBid: number, image: any }[] = [
+const nftData: { id: number, nftName: string, artistName: string, currentBid: number, image: any }[] = [
     {
+        id: 1,
         nftName: "NFT Name",
         artistName: "Artist Name",
         currentBid: 0.4,
         image: { nft01 }
     },
     {
+        id: 2,
         nftName: "NFT Name",
         artistName: "Artist Name",
         currentBid: 0.4,
         image: { nft02 }
     },
     {
+        id: 3,
         nftName: "NFT Name",
         artistName: "Artist Name",
         currentBid: 0.4,
         image: { nft03 }
     },
     {
+        id: 4,
         nftName: "NFT Name",
         artistName: "Artist Name",
         currentBid: 0.4,
         image: { nft04 }
     },
     {
+        id: 5,
         nftName: "NFT Name",
         artistName: "Artist Name",
         currentBid: 0.4,
         image: { nft05 }
     },
     {
+        id: 6,
         nftName: "NFT Name",
         artistName: "Artist Name",
         currentBid: 0.4,
         image: { nft06 }
     },
     {
+        id: 7,
         nftName: "NFT Name",
         artistName: "Artist Name",
         currentBid: 0.4,
         image:  { nft07 }
     },
     {
+        id: 8,
         nftName: "NFT Name",
         artistName: "Artist Name",
         currentBid: 0.4,
         image:  { nft08 }
     },
     {
+        id: 9,
         nftName: "NFT Name",
         artistName: "Artist Name",
         currentBid: 0.4,
         image:  { nft09 }
     },
     {
+        id: 10,
         nftName: "NFT Name",
         artistName: "Artist Name",
         currentBid: 0.4,
