f57f010d466ad65d47a27417c390cc6ecbfea6b6
Behzad Aslani <behzadaslani80@gmail.com>
2023-11-05 18:57:03 +0330

    chore(mockData)
    added images for mockData

---
+++ src/assets/mockData.tsx
@@ -0,0 +1,75 @@
+import nft01 from '../assets/nft01.png'
+import nft02 from '../assets/nft02.png'
+import nft03 from '../assets/nft03.png'
+import nft04 from '../assets/nft04.png'
+import nft05 from '../assets/nft05.png'
+import nft06 from '../assets/nft06.png'
+import nft07 from '../assets/nft07.png'
+import nft08 from '../assets/nft08.png'
+import nft09 from '../assets/nft09.png'
+import nft10 from '../assets/nft10.png'
+
+const nftData: { nftName: string, artistName: string, currentBid: number, image: any }[] = [
+    {
+        nftName: "NFT Name",
+        artistName: "Artist Name",
+        currentBid: 0.4,
+        image: { nft01 }
+    },
+    {
+        nftName: "NFT Name",
+        artistName: "Artist Name",
+        currentBid: 0.4,
+        image: { nft02 }
+    },
+    {
+        nftName: "NFT Name",
+        artistName: "Artist Name",
+        currentBid: 0.4,
+        image: { nft03 }
+    },
+    {
+        nftName: "NFT Name",
+        artistName: "Artist Name",
+        currentBid: 0.4,
+        image: { nft04 }
+    },
+    {
+        nftName: "NFT Name",
+        artistName: "Artist Name",
+        currentBid: 0.4,
+        image: { nft05 }
+    },
+    {
+        nftName: "NFT Name",
+        artistName: "Artist Name",
+        currentBid: 0.4,
+        image: { nft06 }
+    },
+    {
+        nftName: "NFT Name",
+        artistName: "Artist Name",
+        currentBid: 0.4,
+        image:  { nft07 }
+    },
+    {
+        nftName: "NFT Name",
+        artistName: "Artist Name",
+        currentBid: 0.4,
+        image:  { nft08 }
+    },
+    {
+        nftName: "NFT Name",
+        artistName: "Artist Name",
+        currentBid: 0.4,
+        image:  { nft09 }
+    },
+    {
+        nftName: "NFT Name",
+        artistName: "Artist Name",
+        currentBid: 0.4,
+        image:  { nft10 }
+    },
+];
+
+export default nftData
