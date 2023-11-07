7f051c07d81fe868c1b7ee22150af67ee6b25e08
Behzad Aslani <behzadaslani80@gmail.com>
2023-11-07 13:40:44 +0330

    Fix(Navbar) fixed Navbar responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import SubmitPage from "./pages/SubmitPage"
 import Navbar from "./components/Navbar"
-import Footer from "./components/Footer"
+// import Footer from "./components/Footer"
 
 const App = () => {
   return (
@@ -12,7 +12,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/submit/:id" element={<SubmitPage />} />
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </BrowserRouter>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,25 @@
-import Logo from './Logo'
+import logo from '../assets/Logo.svg'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <nav className='flex max-w-[90%] m-auto my-2 mb-12 items-center'>
-            <div className="flex-none mr-12 self-center">
+        <nav className='flex-row md:flex max-w-[80%] m-auto my-4 mb-10 items-center'>
+            <div className="flex md:gap-10 items-center justify-between md:justify-normal w-full mx-auto">
                 <Link to='/'>
-                    <Logo />
+                    <img src={logo} alt="logo" className='max-w-none' />
                 </Link>
-            </div>
-            <form className='flex-auto flex items-center'>
-                <div className="relative mt-3 w-[24rem]">
-                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                        <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                        </svg>
+                <form className='flex items-center'>
+                    <div className="relative max-w-[24rem] md:w-[24rem] justify-self-end">
+                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                            <svg className="w-4 h-4 text-gray-500" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                            </svg>
+                        </div>
+                        <input type="search" id="search-navbar" className="w-full p-2 pl-10 text-sm text-gray-900 border border-none rounded-2xl focus:ring-gray-500 focus:border-gray-500 bg-gray-200" placeholder="Search NFT, Artist, Ehibition..." />
                     </div>
-                    <input type="search" id="search-navbar" className="block w-full p-2 pl-10 text-sm text-gray-900 border border-none rounded-2xl focus:ring-gray-500 focus:border-gray-500 bg-gray-200" placeholder="Search NFT, Artist, Ehibition..." />
-                </div>
-
-            </form>
-            <button className="flex-end bg-[#0E0E0E] transition-all duration-300 hover:bg-gray-300 hover:text-[#0E0E0E] text-white font-bold py-2 px-4 rounded-full justify-self-end">
+                </form>
+            </div>
+            <button className="bg-[#0E0E0E] transition-all duration-300 hover:bg-gray-300 hover:text-[#0E0E0E] text-white font-bold mt-4 md:mt-0 py-2 px-4 rounded-full w-[100%] md:w-[12rem]">
                 Connect Wallet
             </button>
         </nav>
