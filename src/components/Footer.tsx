5953cc752eabf157a63e95cef21bef8ee7d6addd
Behzad Aslani <behzadaslani80@gmail.com>
2023-11-07 14:57:47 +0330

    Fix(Footer)
    fixed footer responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,23 +2,23 @@ import logo_white from '../assets/logo_white.svg'
 
 const Footer = () => {
     return (
-        <footer className="bg-[#0E0E0E] pb-10">
-            <div className="lg:w-[75%] m-auto">
-                <div className=" py-16 flex justify-between">
-                    <img src={logo_white} alt="logo" />
-                    <div className="text-white flex-row w-[32rem]">
+        <footer className="bg-[#0E0E0E] py-10">
+            <div className="w-[80%] lg:w-[80%] m-auto">
+                <div className="md:flex md:justify-between md:mb-4 items-center lg:justify-normal xl:gap-[28rem]">
+                    <img src={logo_white} alt="logo" className='mx-auto md:m-0 mb-4 md:mb-0'/>
+                    <div className="text-white flex-row md:w-[32rem] mb-5 md:mb-0">
                         <h2 className='text-3xl mb-5'>Become an Early Adopter</h2>
-                        <form className='flex gap-2'>
-                            <input type="email" name="email" id="email" className='bg-transparent border-b-2 border-[#5B5B5B] w-full' placeholder='Enter your Email-Adress'/>
-                            <button className="flex-end bg-[#93D39B] hover:bg-gray-300 text-black font-bold py-2 px-10 rounded-full justify-self-end">
+                        <form className='flex gap-2 lg:items-center'>
+                            <input type="email" name="email" id="email" className='bg-transparent border-b-2 border-[#5B5B5B] md:w-full lg:basis-9/12' placeholder='Enter your Email-Adress'/>
+                            <button className="basis-3/12 bg-[#93D39B] hover:bg-gray-300 text-black font-bold w-full md:py-2 md:px-10 rounded-full justify-self-end ">
                                 Submit
                             </button>
                         </form>
                     </div>
                 </div>
-                <div className="text-white flex gap-x-64">
+                <div className="text-white flex-row md:flex md:justify-between">
                     <div className="">
-                        <a href='#' className='text-3xl'>Mintii Account</a>
+                        <h2 className='text-3xl'>Mintii Account</h2>
                         <ul className='text-[#5B5B5B] text-2xl'>
                             <li className='my-2'>
                                 <a href="#">Profile</a>
@@ -35,7 +35,7 @@ const Footer = () => {
                         </ul>
                     </div>
                     <div className="">
-                        <a href='#' className='text-2xl'>Collections</a>
+                        <h2 className='text-2xl'>Collections</h2>
                         <ul className='text-[#5B5B5B] text-2xl'>
                             <li className='my-2'>
                                 <a href="#">Mint NFT</a>
@@ -49,7 +49,7 @@ const Footer = () => {
                         </ul>
                     </div>
                     <div className="">
-                        <a href='#' className='text-2xl'>About Us</a>
+                        <h2 className='text-2xl'>About Us</h2>
                         <ul className='text-[#5B5B5B] text-2xl'>
                             <li className='my-2'>
                                 <a href="#">Mintii</a>
