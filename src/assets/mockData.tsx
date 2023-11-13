dc487dc586d2c6e3e63eca4b1a0c6c10b2c78465
Behzad Aslani <behzadaslani80@gmail.com>
2023-11-13 12:59:26 +0330

    chore: updated mockData
    used fakerjs to generate random names and prices

--- src/assets/mockData.tsx
+++ src/assets/mockData.tsx
@@ -8,77 +8,78 @@ import nft07 from '../assets/nft07.png'
 import nft08 from '../assets/nft08.png'
 import nft09 from '../assets/nft09.png'
 import nft10 from '../assets/nft10.png'
+import { faker } from '@faker-js/faker'
 
 const nftData: { id: number, nftName: string, artistName: string, currentBid: number, image: string }[] = [
     {
         id: 1,
-        nftName: "NFT Name",
-        artistName: "Artist Name",
-        currentBid: 0.4,
+        nftName: "Intentions",
+        artistName: faker.person.fullName(),
+        currentBid: faker.number.float({min: 0.1, max: 0.9, precision: 0.1}),
         image: nft01
     },
     {
         id: 2,
-        nftName: "NFT Name",
-        artistName: "Artist Name",
-        currentBid: 0.4,
+        nftName: "Slide",
+        artistName: faker.person.fullName(),
+        currentBid: faker.number.float({min: 0.1, max: 0.9, precision: 0.1}),
         image: nft02
     },
     {
         id: 3,
-        nftName: "NFT Name",
-        artistName: "Artist Name",
-        currentBid: 0.4,
+        nftName: "Slide House",
+        artistName: faker.person.fullName(),
+        currentBid: faker.number.float({min: 0.1, max: 0.9, precision: 0.1}),
         image: nft03
     },
     {
         id: 4,
-        nftName: "NFT Name",
-        artistName: "Artist Name",
-        currentBid: 0.4,
+        nftName: "Gloom",
+        artistName: faker.person.fullName(),
+        currentBid: faker.number.float({min: 0.1, max: 0.9, precision: 0.1}),
         image: nft04
     },
     {
         id: 5,
-        nftName: "NFT Name",
-        artistName: "Artist Name",
-        currentBid: 0.4,
+        nftName: "Shatters",
+        artistName: faker.person.fullName(),
+        currentBid: faker.number.float({min: 0.1, max: 0.9, precision: 0.1}),
         image: nft05
     },
     {
         id: 6,
-        nftName: "NFT Name",
-        artistName: "Artist Name",
-        currentBid: 0.4,
+        nftName: "Puppet",
+        artistName: faker.person.fullName(),
+        currentBid: faker.number.float({min: 0.1, max: 0.9, precision: 0.1}),
         image: nft06
     },
     {
         id: 7,
-        nftName: "NFT Name",
-        artistName: "Artist Name",
-        currentBid: 0.4,
-        image:  nft07
+        nftName: "Reminiscent",
+        artistName: faker.person.fullName(),
+        currentBid: faker.number.float({min: 0.1, max: 0.9, precision: 0.1}),
+        image: nft07
     },
     {
         id: 8,
-        nftName: "NFT Name",
-        artistName: "Artist Name",
-        currentBid: 0.4,
-        image:  nft08
+        nftName: "Choices",
+        artistName: faker.person.fullName(),
+        currentBid: faker.number.float({min: 0.1, max: 0.9, precision: 0.1}),
+        image: nft08
     },
     {
         id: 9,
-        nftName: "NFT Name",
-        artistName: "Artist Name",
-        currentBid: 0.4,
-        image:  nft09
+        nftName: "Regrets",
+        artistName: faker.person.fullName(),
+        currentBid: faker.number.float({min: 0.1, max: 0.9, precision: 0.1}),
+        image: nft09
     },
     {
         id: 10,
-        nftName: "NFT Name",
-        artistName: "Artist Name",
-        currentBid: 0.4,
-        image:  nft10
+        nftName: "Conquerer",
+        artistName: faker.person.fullName(),
+        currentBid: faker.number.float({min: 0.1, max: 0.9, precision: 0.1}),
+        image: nft10
     },
 ];
 
